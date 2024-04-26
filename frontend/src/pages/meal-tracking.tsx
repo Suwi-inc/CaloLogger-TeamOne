355dca5d5b28f57026761d2f17c89e760393037b
Zeyad Alagamy <wezza2003@gmail.com>
2024-04-27 01:13:00 +0300

    Update container width to 60% in frontend/src/components/container.tsx

--- frontend/src/pages/meal-tracking.tsx
+++ frontend/src/pages/meal-tracking.tsx
@@ -2,22 +2,67 @@ import Container from "../components/container";
 import { getDateMedium, getTimeShort } from "../../utils/parse-time";
 import { MOCK_MEALS_RESPONSE } from "../constants";
 import { Meal } from "../types";
+import { useState } from "react";
 
 const MealEntry = ({ meal_entry }: { meal_entry: Meal }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const { name, date, ingredients, nutritions } = meal_entry;
+
+  const nutritions_key_Mapping = {
+    calories: "Calories",
+    fat_total_g: "Total Fat",
+    fat_saturated_g: "Saturated Fat",
+    protein_g: "Protein",
+    sodium_mg: "Sodium",
+    potassium_mg: "Potassium",
+    cholesterol_mg: "Cholesterol",
+    carbohydrates_total_g: "Total Carbohydrates",
+    fiber_g: "Fiber",
+    sugar_g: "Sugar",
+  };
+
   return (
     <div className="flex flex-col p-5 border border-gray-200 rounded-md gap-5">
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-1">
-          <h1 className="text-lg">{meal_entry.name}</h1>
+          <h1 className="text-lg">{name}</h1>
           <p className="text-sm text-gray-500">
-            {getDateMedium(meal_entry.date)} at {getTimeShort(meal_entry.date)}
+            {getDateMedium(date)} at {getTimeShort(date)}
           </p>
         </div>
-        <p className="text-sm">{meal_entry.ingredients}</p>
+        <p className="text-sm">{ingredients}</p>
       </div>
-      <button className="bg-red-500 text-sm text-white py-2 px-4 rounded-md w-fit">
-        Delete
-      </button>
+      {expanded && (
+        <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-1">
+            <h1 className="text-md font-bold">Nutritions</h1>
+            {Object.entries(nutritions).map(([key, value]) => (
+              <div key={key} className="flex justify-between">
+                <p className="text-sm text-gray-900">
+                  {
+                    nutritions_key_Mapping[
+                      key as keyof typeof nutritions_key_Mapping
+                    ]
+                  }
+                </p>
+                <p>{value}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+      <div className="flex gap-2">
+        <button
+          className="bg-blue-500 text-sm text-white py-2 px-4 rounded-md w-fit"
+          onClick={() => setExpanded(!expanded)}
+        >
+          {expanded ? "Hide" : "Show"} Nutritions
+        </button>
+        <button className="bg-red-500 text-sm text-white py-2 px-4 rounded-md w-fit">
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
