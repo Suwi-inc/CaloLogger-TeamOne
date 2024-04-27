525b36ae12b0118b4e0c8e810aa20d919e77fac5
Zeyad Alagamy <wezza2003@gmail.com>
2024-04-27 22:07:16 +0300

    Fix loading state and error handling in MealChart component

--- frontend/src/tests/pages/dashboard/meal-chart.test.tsx
+++ frontend/src/tests/pages/dashboard/meal-chart.test.tsx
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi } from "vitest";
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import MealChart from "../../../pages/dashboard/meal-chart";
 import { MOCK_MEALS_RESPONSE } from "../../constants";
 
@@ -9,31 +9,31 @@ vi.mock("react-chartjs-2", () => ({
 }));
 
 describe("MealChart", () => {
-  beforeAll(() => {
+  beforeEach(() => {
     global.fetch = vi.fn().mockResolvedValue({
       ok: true,
       json: async () => MOCK_MEALS_RESPONSE,
     });
-    //   .mockRejectedValue(new Error("Failed to fetch data"));
   });
 
-  it("displays error message when there is an error", () => {
+  it("displays error message when there is an error", async () => {
+    global.fetch = vi.fn().mockRejectedValue(new Error("Failed to fetch data"));
     render(<MealChart />);
-    expect(screen.getByText("Error: Failed to fetch data")).toBeInTheDocument();
-  });
-  it("displays loading state correctly", () => {
-    render(<MealChart />);
-    expect(screen.getByText("Loading...")).toBeInTheDocument();
+    expect(
+      await screen.findByText("Error: Failed to fetch data")
+    ).toBeInTheDocument();
   });
 
-  it("renders the chart correctly when data is available", () => {
+  it("displays loading state correctly", async () => {
     render(<MealChart />);
-    waitFor(
-      () => {
-        expect(screen.getByText("Meal Calories Chart")).toBeInTheDocument();
-        expect(screen.getByRole("img")).toBeInTheDocument();
-      },
-      { timeout: 1000 }
+    expect(screen.getByText("Loading...")).toBeInstanceOf(HTMLParagraphElement);
+  });
+
+  it("renders the chart correctly when data is available", async () => {
+    render(<MealChart />);
+    expect(await screen.findByText("Meal Calories Chart")).toBeInstanceOf(
+      HTMLHeadingElement
     );
+    expect(document.getElementsByTagName("canvas")[0]).toBeInTheDocument();
   });
 });
