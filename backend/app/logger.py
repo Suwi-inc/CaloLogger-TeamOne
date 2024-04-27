fe4d522e33a18a9bb57520f5cb6b82a085e83f2f
ahmedXDR <a.soliman@innopolis.university>
2024-04-27 20:25:07 +0300

    [fix] stop loguru from logging to stdout

--- backend/app/logger.py
+++ backend/app/logger.py
@@ -1,47 +1,11 @@
-from sys import stdout
-import logging
-
 from loguru import logger
 
-import google.cloud.logging
-from google.cloud.logging_v2.handlers import CloudLoggingHandler
-from google.auth.credentials import with_scopes_if_required
 
-config = {
-    "handlers": [
-        {
-            "sink": stdout,
-            "level": "INFO",
-            "format": "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | \
-                <level>{level: <8}</level> | <level>{message}</level>",
-        },
-        {
-            "sink": "logs/app.log",
-            "serialize": True,
-            "rotation": "1 day",
-            "retention": "30 days",
-            "level": "DEBUG",
-        },
-    ],
-}
-
-
-def setup_logger(
-    name: str = "my_logger",
-    level: int = logging.DEBUG,
-    project_id: str = "my_project",
-):
-    logger.remove()
-    logger.configure(**config)
-
-    # Cloud Logging:
-    credentials = with_scopes_if_required(
-        None, ["https://www.googleapis.com/auth/cloud-platform"]
+def setup_logger():
+    logger.add(
+        sink="logs/app.log",
+        serialize=True,
+        rotation="1 day",
+        retention="30 days",
+        level="DEBUG",
     )
-    client = google.cloud.logging.Client(
-        project=project_id,
-        credentials=credentials,
-    )
-    handler = CloudLoggingHandler(client, name=name)
-    handler.setLevel(level=level)
-    logger.add(handler)
