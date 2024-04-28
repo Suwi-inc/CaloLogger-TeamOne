d4f48f7bcd8d69ef6c783fd0ce59f408078c5f32
Mosab Mohamed <mosab.f.r@gmail.com>
2024-04-28 04:49:19 +0300

    fix codesmells detected by sonarqube

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,6 @@ from sqlalchemy.orm import sessionmaker
 
 os.makedirs("db", exist_ok=True)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./db/test.sqlite"  # SQLite database file
-# SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./test.db"  # Use for async
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
